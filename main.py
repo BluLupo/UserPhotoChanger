a4cd93e441bc56530d2a8d920352ee7e9ee73db7
BluLupo <hersel1991@gmail.com>
2021-12-29 00:26:04 +0100

    fix manual command

--- main.py
+++ main.py
@@ -18,12 +18,10 @@ app = Client(
 )
 
 # Constants for Scheduler
-HOUR = 12
+HOUR = 22
 MINUTE = 30
 SECOND = 00
 
-# /rand Manual Random
-@app.on_message(filters.command("rand") & filters.private & filters.user(Config.OWNER))
 def rand_command():
     current_time = datetime.datetime.utcnow()
     directory = "img/"
@@ -31,6 +29,14 @@ def rand_command():
     app.set_profile_photo(photo=open(directory + random_image,'rb'))
     print("Image modified on {}".format(current_time))
 
+# /rand Manual Random
+@app.on_message(filters.command("random") & filters.private & filters.user(Config.OWNER))
+def manual_rand_command(client,message):
+    current_time = datetime.datetime.utcnow()
+    directory = "img/"
+    random_image = random.choice(os.listdir(directory))
+    app.set_profile_photo(photo=open(directory + random_image,'rb'))
+    print("Image modified on {}".format(current_time))
 
 scheduler = AsyncIOScheduler()
 scheduler.add_job(rand_command, 'cron', hour=HOUR, minute=MINUTE, second=SECOND)
